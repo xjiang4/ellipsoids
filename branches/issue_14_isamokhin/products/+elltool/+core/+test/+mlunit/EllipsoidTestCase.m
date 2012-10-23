9ce83f879173c66fc75dabc662299983e86cea13
igorian.vmk@gmail.com <igorian.vmk@gmail.com@fbfa2205-8925-0410-8e01-63a555b7045b>
2012-10-23 11:02:30 +0000

    Working with: test for isinside test for distance

--- branches/issue_14_isamokhin/products/+elltool/+core/+test/+mlunit/EllipsoidTestCase.m
+++ branches/issue_14_isamokhin/products/+elltool/+core/+test/+mlunit/EllipsoidTestCase.m
@@ -10,34 +10,56 @@ classdef EllipsoidTestCase < mlunitext.test_case
         end
         
         function self = testIsInside(self)
-            E = ellipsoid([2; 1], [4, 1; 1, 1])
-            B = ell_unitball(2)
-            testRes = isinside(E, [E B], 'i')
+            %figure(1)
+            %hold on;
+            E = ellipsoid([2; 1], [4, 1; 1, 1]);
+            B = ell_unitball(2);
+            testRes = isinside(E, [E B], 'i');
             mlunit.assert_equals(1, testRes);
-%             figure(2);
-%             hold on;
-%             E = ellipsoid([2; 1], [4, 1; 1, 1]);
-%             plot(E);
-%             B = ell_unitball(2);
-%             plot(B);
-%             testRes = isinside(E, [E B], 'u');
-%             mlunit.assert_equals(0, testRes);
-%             figure(3);
-%             hold on;
-%             E = ellipsoid([2; 1; 0], [4, 1, 1; 1, 2, 1; 1, 1, 5]);
-%             plot(E);
-%             B = ell_unitball(3);
-%             plot(B);
-%             testRes = isinside(E, [E B], 'i');
-%             mlunit.assert_equals(1, testRes);
-%             figure(3);
-%             hold on;
-%             E = ellipsoid([2; 1; 0], [4, 1, 1; 1, 2, 1; 1, 1, 5]);
-%             plot(E);
-%             B = ell_unitball(3);
-%             plot(B);
-%             testRes = isinside(E, [E B], 'u');
-%             mlunit.assert_equals(0, testRes);
+            %figure(2);
+            %hold on;
+            E = ellipsoid([2; 1], [4, 1; 1, 1]);
+            %plot(E);
+            B = ell_unitball(2);
+            %plot(B);
+            testRes = isinside(E, [E B], 'u');
+            mlunit.assert_equals(0, testRes);
+            %figure(3);
+            %hold on;
+            %rotate3d;
+            E = ellipsoid([2; 1; 0], [4, 1, 1; 1, 2, 1; 1, 1, 5]);
+            %plot(E, 'c');
+            B = ell_unitball(3);
+            %plot(B);
+            testRes = isinside(E, [E B], 'i');
+            mlunit.assert_equals(1, testRes);
+            %figure(4);
+            %hold on;
+            %rotate3d;
+            E = ellipsoid([2; 1; 0], [4, 1, 1; 1, 2, 1; 1, 1, 5]);
+            %plot(E, 'c');
+            B = ell_unitball(3);
+            %plot(B);
+            testRes = isinside(E, [E B], 'u');
+            mlunit.assert_equals(0, testRes);
+            %figure(5);
+            %hold on;
+            %rotate3d;
+            E = ellipsoid([5; 5; 5], [4, 1, 1; 1, 2, 1; 1, 1, 5]);
+            %plot(E, 'c');
+            B = ell_unitball(3);
+            %plot(B);
+            testRes = isinside(E, [E B], 'u');
+            mlunit.assert_equals(-1, testRes);
         end
+        
+        function self = testDistance(self)
+            testRes = distance([0;0], [1; 0]);
+            mlunit.assert_equals(1, testRes);
+        end
+        
+        
+        
+        
     end
 end
