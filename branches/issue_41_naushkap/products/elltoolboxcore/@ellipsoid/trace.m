0c1dce67179041a006b12dcfefda5c99f18600a3
N.Aushkap@gmail.com <N.Aushkap@gmail.com@fbfa2205-8925-0410-8e01-63a555b7045b>
2012-11-21 20:50:58 +0000

    issue 41 create new branch for easier merge

---
+++ branches/issue_41_naushkap/products/elltoolboxcore/@ellipsoid/trace.m
@@ -0,0 +1,41 @@
+function T = trace(E)
+%
+% TRACE - returns the trace of the ellipsoid.
+%
+%
+% Description:
+% ------------
+%
+%    T = TRACE(E)  Computes the trace of ellipsoids in ellipsoidal array E.
+%
+%
+% Output:
+% -------
+%
+%    T - array of trace values, same size as E.
+%
+%
+% See also:
+% ---------
+%
+%    ELLIPSOID/ELLIPSOID.
+%
+%
+% Author:
+% -------
+%
+%    Alex Kurzhanskiy <akurzhan@eecs.berkeley.edu>
+%
+
+  [m, n] = size(E);
+  T      = [];
+
+  for i = 1:m
+    t = [];
+    for j = 1:n
+      t = [t trace(double(E(i, j)))];
+    end
+    T = [T; t];
+  end
+
+  return;
