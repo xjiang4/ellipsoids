316125fa0192e833114a3270a90d674153b7b3e5
N.Aushkap@gmail.com <N.Aushkap@gmail.com@fbfa2205-8925-0410-8e01-63a555b7045b>
2012-11-22 14:34:10 +0000

    issue 41 commit after merge with trunk

--- branches/issue_41_naushkap/products/elltoolboxcore/@ellipsoid/dimension.m
+++ branches/issue_41_naushkap/products/elltoolboxcore/@ellipsoid/dimension.m
@@ -20,6 +20,8 @@ function [spaceDimMat, ellDimMat] = dimension(myEllMat)
   import elltool.conf.Properties;
 
   [mRows, nCols] = size(myEllMat);
+  spaceDimMat = zeros(mRows, nCols);
+  ellDimMat = zeros(mRows, nCols);
   
   for iRows = 1:mRows
     for jCols = 1:nCols
