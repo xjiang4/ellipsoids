800de9f2020ac05dcb2b210a4fb56ccc7bed8b5e
vetbar42@gmail.com <vetbar42@gmail.com@fbfa2205-8925-0410-8e01-63a555b7045b>
2013-03-27 12:13:23 +0000

    Issue 39. Deleting branch.

--- branches/issue_39_vbaranov/products/+elltool/+reach/+test/run_tests.m
+++
@@ -1,7 +0,0 @@
-function result = run_tests(varargin)
-    runner = mlunitext.text_test_runner(1, 1);
-    suite =mlunitext.test_suite.fromTestCaseNameList({...
-        'elltool.reach.test.mlunit.NewReachTestCase',...
-        'elltool.reach.test.mlunit.ReachTestCase'},varargin);
-    %
-result = runner.run(suite);
--- branches/issue_39_vbaranov/products/+gras/+ellapx/+test/run_tests.m
+++
@@ -1,4 +0,0 @@
-function results=run_tests(varargin)
-resList{1}=gras.ellapx.uncertcalc.test.run_tests(varargin{:});
-resList{2}=gras.ellapx.smartdb.test.run_tests();
-results=[resList{:}];
