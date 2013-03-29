4bb7b7f584d2a2960d4241f7e476111936b6283b
noblesseklo@gmail.com <noblesseklo@gmail.com@fbfa2205-8925-0410-8e01-63a555b7045b>
2013-03-29 10:22:57 +0000

    Issue #74 Creating branch.

---
+++ branches/issue_74_dkozlov/products/+elltool/listconf.m
@@ -0,0 +1,16 @@
+function listconf()
+%LISTCONF gives a list of existing configurations
+%
+%No input or output, just displays list of configuration
+%
+%$Author: Zakharov Eugene  <justenterrr@gmail.com> $    $Date: 17-november-2012 $
+%$Copyright: Moscow State University,
+%            Faculty of Computational Mathematics and Computer Science,
+%            System Analysis Department 2012 $
+%
+import elltool.logging.Log4jConfigurator;
+logger=Log4jConfigurator.getLogger();
+
+confRepoMgr=elltool.conf.Properties.getConfRepoMgr();
+confList=confRepoMgr.getConfNameList();
+logger.info([sprintf('%s, ',confList{1:end-1}), confList{end}]);
