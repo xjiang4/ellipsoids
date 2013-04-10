2f58f06e4cbef3658851bb493d52171c918f6da6
vetbar42@gmail.com <vetbar42@gmail.com@fbfa2205-8925-0410-8e01-63a555b7045b>
2013-04-10 11:02:43 +0000

    Issue 92. Deleting branch.

--- branches/issue_92_vbaranov/products/+elltool/+exttbx/IExtTBXController.m
+++
@@ -1,8 +0,0 @@
-classdef (Abstract) IExtTBXController  
-    methods(Abstract,Static)
-        fullSetup(self,arg)
-        isOnPath(self)
-        checkIfSetUp(self)
-        checkIfOnPath(self)
-    end
-end
