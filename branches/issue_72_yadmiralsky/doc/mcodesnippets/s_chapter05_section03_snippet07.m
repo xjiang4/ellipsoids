f302c88479a108ac95dac6f869f0662af1465907
swige.ide <swige.ide@fbfa2205-8925-0410-8e01-63a555b7045b>
2013-05-04 09:41:09 +0000

    issue #72 Branch was deleted.

--- branches/issue_72_yadmiralsky/doc/mcodesnippets/s_chapter05_section03_snippet07.m
+++
@@ -1,7 +0,0 @@
-externallEllMat = firstRsObj.get_ea()  % external approximating ellipsoids
-
-% externallEllMat =
-% 2x100 array of ellipsoids.
-
-% internal approximating ellipsoids
-[internalEllMat, timeVec] = firstRsObj.get_ia();  
