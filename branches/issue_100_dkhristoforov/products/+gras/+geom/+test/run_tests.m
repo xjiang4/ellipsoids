687a69bf6d0d01b3755aea697ec9fec71b7c12cd
DmitryKh92@gmail.com <DmitryKh92@gmail.com@fbfa2205-8925-0410-8e01-63a555b7045b>
2013-05-08 11:10:37 +0000

    Issue 100: deleting branch

--- branches/issue_100_dkhristoforov/products/+gras/+geom/+test/run_tests.m
+++
@@ -1,5 +0,0 @@
-function results=run_tests(varargin)
-resCell{3}=gras.geom.ell.test.run_tests(varargin{:});
-resCell{2}=gras.geom.tri.test.run_tests(varargin{:});
-resCell{1}=gras.geom.sup.test.run_tests(varargin{:});
-results=[resCell{:}];
