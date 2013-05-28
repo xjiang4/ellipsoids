7552e836c0e6532e4b9c9561036e6019505fc150
justenterrr <justenterrr@fbfa2205-8925-0410-8e01-63a555b7045b>
2013-05-28 07:42:45 +0000

    issue 93 delete branch

--- branches/issue_93_ezakharov/doc/mcodesnippets/s_chapter06_section04_snippet04.m
+++
@@ -1,6 +0,0 @@
-crsObjVec = [];
-for i = 1:size(D, 2)
-     rsObj = elltool.reach.ReachDiscrete(secSys, intersectEllVec(D(i)), ...
-             dirsMat, [D(i)-1 nSteps]);
-     crsObjVec = [crsObjVec rsObj];
-end
