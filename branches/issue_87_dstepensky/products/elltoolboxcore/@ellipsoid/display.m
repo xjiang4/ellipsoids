ecc5b8055f461a7e214e39a3a5973730f5a68ad2
reinkarn@gmail.com <reinkarn@gmail.com@fbfa2205-8925-0410-8e01-63a555b7045b>
2013-05-30 11:01:06 +0000

    Issue 87 Created branch

---
+++ branches/issue_87_dstepensky/products/elltoolboxcore/@ellipsoid/display.m
@@ -0,0 +1,61 @@
+function display(myEllMat)
+%
+% DISPLAY - Displays the details of the ellipsoid object.
+%
+% Input:
+%   regular:
+%       myEllMat: ellipsoid [mRows, nCols] - matrix of ellipsoids.
+%           
+% Example:
+%   ellObj = ellipsoid([-2; -1], [2 -1; -1 1]);
+%   display(ellObj)
+% 
+%   ellObj =
+% 
+%   Center:
+%       -2
+%       -1
+% 
+%   Shape Matrix:
+%        2    -1
+%       -1     1
+% 
+%   Nondegenerate ellipsoid in R^2.
+% 
+% 
+% $Author: Alex Kurzhanskiy <akurzhan@eecs.berkeley.edu>
+% $Copyright: The Regents of the University of California 
+%             2004-2008 $
+
+fprintf('\n');
+disp([inputname(1) ' =']);
+
+sizeVec = size(myEllMat);
+nDims = numel(sizeVec);
+isEmpty = isempty(myEllMat);
+if (nDims > 1 && (sizeVec(1) > 1 || sizeVec(2) > 1) )|| isEmpty
+    if isEmpty
+        fprintf('Empty array of ellipsoids with dimensionality ');
+    else
+        fprintf('Array of ellipsoids with dimensionality ');
+    end
+    for iDim = 1:nDims-1
+        fprintf('%dx', sizeVec(iDim));
+    end
+    fprintf('%d\n\n', sizeVec(nDims));
+else
+    fprintf('\n');
+    fprintf('Center:\n'); disp(myEllMat.centerVec);
+    fprintf('Shape Matrix:\n'); disp(myEllMat.shapeMat);
+    if myEllMat.isEmpty()
+        fprintf('Empty ellipsoid.\n\n');
+    else
+        [spaceDim, ellDim]    = dimension(myEllMat);
+        if ellDim < spaceDim
+            fprintf('Degenerate (rank %d) ellipsoid in R^%d.\n\n', ...
+                ellDim, spaceDim);
+        else
+            fprintf('Nondegenerate ellipsoid in R^%d.\n\n', spaceDim);
+        end
+    end
+end
