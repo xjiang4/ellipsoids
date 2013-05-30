b84f4d1315f2bf2cd8a5b83e580e0066198f52c3
kirill.mayantsev@gmail.com <kirill.mayantsev@gmail.com@fbfa2205-8925-0410-8e01-63a555b7045b>
2013-05-30 21:45:48 +0000

    Issue 94 Delete branch

--- branches/issue_94_kmayantsev/doc/mcodesnippets/s_chapter06_section04_snippet04.m
+++
@@ -1,6 +0,0 @@
-crsObjVec = [];
-for i = 1:size(dVec, 2)
-     rsObj = elltool.reach.ReachDiscrete(secSys, intersectEllVec(dVec(i)), ...
-             dirsMat, [dVec(i)-1 nSteps]);
-     crsObjVec = [crsObjVec rsObj];
-end
