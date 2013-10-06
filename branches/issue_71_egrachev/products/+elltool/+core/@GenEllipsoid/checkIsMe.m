687f713d8867aea315b6921f7713fb36b707821f
egorgrachev.msu@gmail.com <egorgrachev.msu@gmail.com@fbfa2205-8925-0410-8e01-63a555b7045b>
2013-10-06 22:14:44 +0000

    Issue 71 Enhancement: local changes and adding new files

---
+++ branches/issue_71_egrachev/products/+elltool/+core/@GenEllipsoid/checkIsMe.m
@@ -0,0 +1,33 @@
+function checkIsMe(ellArr,varargin)
+%
+% CHECKISME - determine whether input object is generalized ellipsoid. 
+%             And display message and abort function if input object
+%             is not generalized ellipsoid
+%
+% Input:
+%   regular:
+%       someObjArr: any[] - any type array of objects.
+%
+% Example:
+%   ellObj = elltool.core.GenEllipsoid([1; 2], eye(2));
+%   elltool.core.GenEllipsoid.checkIsMe(ellObj)
+% 
+% 
+% % $Author: Rustam Galiev <glvrst@gmail.com> $	$Date: 2012-12-27 $ 
+% % $Author: Egor Grachev <egorgrachev.msu@gmail.com> $ $Date: 04-10-2013$
+% $Copyright: Moscow State University,
+%            Faculty of Applied Mathematics and Computer Science,
+%            System Analysis Department 2012 $
+    nArgIn = nargin;
+    if nArgIn == 1
+        modgen.common.checkvar(ellArr,@(x) isa(x,'elltool.core.GenEllipsoid'),...
+            'errorTag','wrongInput',...
+            'errorMessage','input argument must be elltool.core.GenEllipsoid.');
+    elseif nArgIn==2
+        modgen.common.checkvar(ellArr,@(x) isa(x,'elltool.core.GenEllipsoid'),...
+            'errorTag','wrongInput','errorMessage',...
+            [varargin{1}]);%, ' input argument must be elltool.core.GenEllipsoid.']);
+    else
+        modgen.common.checkvar(ellArr,@(x) isa(x,'elltool.core.GenEllipsoid'),varargin{:});
+    end
+end
---
+++ branches/issue_71_egrachev/products/+elltool/+core/@GenEllipsoid/getCopy.m
@@ -0,0 +1,47 @@
+function copyEllArr = getCopy(ellArr)
+% GETCOPY - gives array the same size as ellArr with copies of elements of 
+%           ellArr.
+%
+% Input:
+%   regular:
+%       ellArr: elltool.core.GenEllipsoid[nDim1, nDim2,...] - multidimensional 
+%       array of generalized ellipsoids.
+%
+% Output:
+%   copyEllArr: elltool.core.GenEllipsoid[nDim1, nDim2,...] - multidimension array of  
+%       copies of elements of ellArr.
+% 
+% Example:
+%   firstEllObj = elltool.core.GenEllipsoid([-1; 1], [2 0; 0 3]);
+%   secEllObj = elltool.core.GenEllipsoid([1; 2], eye(2));
+%   ellVec = [firstEllObj secEllObj];
+%   copyEllVec = getCopy(ellVec)
+% 
+%   copyEllVec =
+%   1x2 array of generalized ellipsoids.
+% 
+%
+% $Author: Kirill Mayantsev  <kirill.mayantsev@gmail.com> $  $Date: March-2013 $
+% $Author: Peter Gagarinov  <pgagarinov@gmail.com> $  $Date: 24-04-2013 $
+% $Author: Egor Grachev <egorgrachev.msu@gmail.com> $ $Date: 04-10-2013 $
+% $Copyright: Moscow State University,
+%            Faculty of Computational Mathematics and Computer Science,
+%            System Analysis Department 2013 $
+%
+if isempty(ellArr)
+    copyEllArr = elltool.core.GenEllipsoid.empty(size(ellArr));
+elseif isscalar(ellArr)
+    copyEllArr=elltool.core.GenEllipsoid();
+    fSingleCopy(copyEllArr,ellArr);
+else
+    sizeCVec = num2cell(size(ellArr));
+    copyEllArr(sizeCVec{:}) = elltool.core.GenEllipsoid();
+    arrayfun(@fSingleCopy,copyEllArr,ellArr);
+end
+    function fSingleCopy(copyEll,ell)
+        copyEll.centerVec = ell.centerVec;
+        %copyEll.shapeMat = ell.shapeMat;
+        copyEll.diagMat = ell.diagMat;
+        copyEll.eigvMat = ell.eigvMat;
+    end
+end
