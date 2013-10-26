291e73e9d19b03ecbad6939055d6e8f4dfd2c634
SeregaDrozh@gmail.com <SeregaDrozh@gmail.com@fbfa2205-8925-0410-8e01-63a555b7045b>
2013-10-26 10:56:07 +0000

    Issue 136 Enhancement: Modify gras.interp.* classes,  moved gras.interp into gras.mat.interp, moved gras.mat.symb into gras.mat.fcnlib

--- branches/issue_136_sdrozhzhin/products/+gras/+mat/+interp/MatrixRowCubicSpline.m
+++ branches/issue_136_sdrozhzhin/products/+gras/+mat/+interp/MatrixRowCubicSpline.m
@@ -1,4 +1,4 @@
-classdef MatrixRowCubicSpline<gras.interp.AMatrixCubicSpline
+classdef MatrixRowCubicSpline<gras.mat.interp.AMatrixCubicSpline
     % $Author: Peter Gagarinov  <pgagarinov@gmail.com> $	$Date: 2011-08$
     % $Copyright: Moscow State University,
     %            Faculty of Computational Mathematics and Computer Science,
@@ -6,7 +6,7 @@ classdef MatrixRowCubicSpline<gras.interp.AMatrixCubicSpline
     %    
     methods (Access=protected,Static)
         function ppFormList=buildSplineCoeffs(dataArray,timeVec)
-            import gras.interp.AMatrixCubicSpline;
+            import gras.mat.interp.AMatrixCubicSpline;
             [~,nDims,nRows]=AMatrixCubicSpline.getSizeProps(dataArray);
             ppFormList=cell(1,nRows);
             switch nDims
@@ -22,25 +22,25 @@ classdef MatrixRowCubicSpline<gras.interp.AMatrixCubicSpline
         end
     end
     methods (Access=public)
-        function resArray=evaluate(self,timeVec)
-            mSizeVec=self.mSizeVec;
-            mSizeLen=length(mSizeVec);
-            nDims=2-(any(mSizeVec == 1) || (mSizeLen < 2));
-            nRows=self.nRows;
-            nCols=self.nCols;
-            nTimePoints=length(timeVec);
-            switch nDims
-                case 1
-                    resArray=zeros(nRows,nTimePoints);
-                    for k=1:1:nRows
-                        resArray(k,:)=fnval(self.ppFormList{k},timeVec);
+        function resArray = evaluate(self, timeVec)
+            %mSizeVec=self.mSizeVec;
+            %mSizeLen=length(mSizeVec);
+            %nDims=2-(any(mSizeVec == 1) || (mSizeLen < 2));
+            nRows = self.nRows;
+            nCols = self.nCols;
+            nTimePoints = length(timeVec);
+           % switch nDims
+            %    case 1
+             %       resArray=zeros(nRows,nTimePoints);
+              %      for k=1:1:nRows
+               %         resArray(k,:)=fnval(self.ppFormList{k},timeVec);
+                %    end
+                %case 2
+                    resArray = zeros(nRows, nCols, nTimePoints);
+                    for k = 1 : 1 : nRows
+                        resArray(k, :, :) = fnval(self.ppFormList{k}, timeVec);
                     end
-                case 2
-                    resArray=zeros(nRows,nCols,nTimePoints);
-                    for k=1:1:nRows
-                        resArray(k,:,:)=fnval(self.ppFormList{k},timeVec);
-                    end
-            end
+            %end
         end
     end
     methods
@@ -55,7 +55,7 @@ classdef MatrixRowCubicSpline<gras.interp.AMatrixCubicSpline
             %       timeVec: double[1,nTimePoints] -
             %           
             %
-            self=self@gras.interp.AMatrixCubicSpline(varargin{:});
+            self=self@gras.mat.interp.AMatrixCubicSpline(varargin{:});
         end
     end
 end
