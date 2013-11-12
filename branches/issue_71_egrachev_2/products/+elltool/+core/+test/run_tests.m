3b054ae86fb6b77d7094ffdbeffb8d9079a7a8a6
egorgrachev.msu@gmail.com <egorgrachev.msu@gmail.com@fbfa2205-8925-0410-8e01-63a555b7045b>
2013-11-12 15:05:04 +0000

    removing old branch

--- branches/issue_71_egrachev_2/products/+elltool/+core/+test/run_tests.m
+++
@@ -1,4 +0,0 @@
-function result=run_tests(varargin)
-    %resList{1} = elltool.core.test.run_tests_by_factory('ellipsoid');
-    resList{2} = elltool.core.test.run_tests_by_factory('GenEllipsoid');
-    result=[resList{:}];
--- branches/issue_71_egrachev_2/products/elltoolboxcore/@ellipsoid/isMe.m
+++
@@ -1,3 +0,0 @@
-function res = isMe(ellObj)
-    res = isa(ellObj, 'ellipsoid');
-end
