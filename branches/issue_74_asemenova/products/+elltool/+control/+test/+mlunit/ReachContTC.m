c5c47ac57078d1089bcc15f1f61b21d3c6d34193
anastasia.semenova424@gmail.com <anastasia.semenova424@gmail.com@fbfa2205-8925-0410-8e01-63a555b7045b>
2013-12-29 17:37:51 +0000

    issue 74 Enhancement: Now only tests with A=0 work correctly. If A<>0 there are problems.

--- branches/issue_74_asemenova/products/+elltool/+control/+test/+mlunit/ReachContTC.m
+++ branches/issue_74_asemenova/products/+elltool/+control/+test/+mlunit/ReachContTC.m
@@ -38,34 +38,39 @@ classdef ReachContTC < mlunitext.test_case
             switchSysTimeVec=self.reachObj.getSwitchTimeVec();
             switchTimeVecLenght=numel(switchSysTimeVec);
             intProbDynamicsList=self.reachObj.getIntProbDynamicsList();            
-            %goodDirSetList=self.reachObj.getGoodDirSetList();
+            goodDirSetList=self.reachObj.getGoodDirSetList();
             intEllTube=ellTubeRel.getTuplesFilteredBy('approxType', gras.ellapx.enums.EApproxType.Internal);
             options = odeset('RelTol',1e-4,'AbsTol',1e-4);
-            %isBackward=self.reachObj.isbackward();
-            %there are 3 cases for x0
-%             plotByIa(self.reachObj);
-%             plotByEa(self.reachObj);
-            %plotIa(self.reachObj);
+
             nTuples = intEllTube.getNTuples();
-           %%% x0Mat(:,1)=[5; -7]; %%rot2d
-            x0Mat(:,1)=[0.5; 0.5];
-            x0Mat(:,2)=[0;1];
-%             x0Mat(:,3)=[-3;0.4]; %Interval
-            %x0Mat(:,3)=[-1;2];
-            %x0Mat(:,4)=[4;1.5];
+            x0Mat(:,1)=[1;2]; %%% check
+            %x0Mat(:,1)=[100; 29]; %%third
+            x0Mat(:,1)=[5; -7]; %%rot2d
+            
+            x0Mat(:,2)=[10.5; 0]; %%rot2d
+            x0Mat(:,3)=[-7; -7]; %%rot2d
+% %          x0Mat(:,1)=[0.5; 0.5];
+% %          x0Mat(:,2)=[0;1];
+%            x0Mat(:,1)=[-3;0.4]; %Interval
+%            x0Mat(:,2)= [1;0.7]; %Interval
+% %             
+%            x0Mat(:,3)= [0.4; 0.4]; %Interval
+%            x0Mat(:,4)= [5; 0]; %Interval  1.002000 вне границы 1.07-6008
+           % x0Mat(:,3)=[-1;2];
+          % x0Mat(:,1)=[4;1.5];
             for iXCount=1:size(x0Mat,2)
                 x0Vec=x0Mat(:,iXCount)
                 %x0Vec=[50; 10];
                 %particular example (there aren't any switches)
                 %x0Vec=[1.7; 1.3; -11.1; -11.6];
                 isX0inSet=false;
-
+ 
                 controlObj=elltool.control.ContControlBuilder(self.reachObj);
                 controlFuncObj=controlObj.getControl(x0Vec);
                 if (~all(size(x0Vec)==size(intEllTube.aMat{1}(:,1))))
                     self.runAndCheckError('controlObj.getControl(x0Vec)','wrongInput');
                     return;
-                end
+                end  
                 for iTube=1:nTuples
                     %check if x is in E(q,Q), x: <x-q,Q^(-1)(x-q)><=1
                     %if (dot(x-qVec,inv(qMat)*(x-qVec))<=1)
@@ -97,21 +102,25 @@ classdef ReachContTC < mlunitext.test_case
                     iSwitchBack=switchTimeVecLenght-iSwitch;               
                     bpVec=intProbDynamicsList{iSwitchBack}{iTube}.getBptDynamics();
                     bpbMat=intProbDynamicsList{iSwitchBack}{iTube}.getBPBTransDynamics();
-    %                 
+    %                  
                     t0=switchSysTimeVec(iSwitch);
                     t1=switchSysTimeVec(iSwitch+1);
                     ind=find(ellTubeRel.timeVec{iTube}==t1);
                     AtMat=intProbDynamicsList{iSwitchBack}{iTube}.getAtDynamics();
 
-                    [T,Y] = ode45(@(t,y)ode(t,y,AtMat,controlFuncObj,bpVec,bpbMat),[t0 t1],x0Vec',options);
+                    %[T,Y] = ode45(@(t,y)ode(t,y,AtMat,controlFuncObj,bpVec,bpbMat),[t0 t1],x0Vec',options);
+                    h=10^(-3);
+                    f=@(t,y)-AtMat.evaluate(t1-t+t0)*y'+controlFuncObj.evaluate(y',t);
+                    %f=@(t,y)AtMat.evaluate(t)*y'+controlFuncObj.evaluate(y',t);
+                    Y=eiler(h,t0,t1,x0Vec',f);
                     q1Vec=ellTubeRel.aMat{iTube}(:,ind); 
                     q1Mat=ellTubeRel.QArray{iTube}(:,:,ind);
-
+  
                     if (dot(Y(end,:)'-q1Vec,q1Mat\(Y(end,:)'-q1Vec))>1)
                         isCurrentEqual=false;
                     end
 
-
+ 
                 end
                 q1Vec=ellTubeRel.aMat{iTube}(:,end);
                 q1Mat=ellTubeRel.QArray{iTube}(:,:,end);
@@ -135,13 +144,26 @@ classdef ReachContTC < mlunitext.test_case
                isCurEqual=true;
                dy=zeros(AtMat.getNRows(),1); 
                dy=AtMat.evaluate(t)*y+controlFuncVec.evaluate(y,t);
-            
-%                ump0Vec=controlFuncVec.evaluate(y,t)-bpVec.evaluate(t);
-%                if (dot(ump0Vec,bpbMat.evaluate(t)\ump0Vec)>1)
-%                    isCurEqual=false;
-%                end
-               % return or events
+
             end
-        end        
+
+
+            function y=eiler(h,x0,x1,y0,f)
+            %     if frac((x1-x0)/h)
+            %         trunc((x1-x0)/h)+1;
+            %     end
+                i=1;
+                xInd=x0;
+                y(1,:)=y0';
+                while (xInd<x1)
+                    y(i+1,:)=y(i,:)+h*f(xInd,y(i,:))';
+                    xInd=xInd+h;
+                    i=i+1;
+
+                end
+            end
+
+        end  
+        
     end
 end
--- branches/issue_74_asemenova/products/+elltool/+control/+test/run_control_tests.m
+++ branches/issue_74_asemenova/products/+elltool/+control/+test/run_control_tests.m
@@ -10,20 +10,22 @@ crm=gras.ellapx.uncertcalc.test.regr.conf.ConfRepoMgr();
 crmSys=gras.ellapx.uncertcalc.test.regr.conf.sysdef.ConfRepoMgr();
 %
 confCMat = {
+%     'check', [1 0]; %0 1 1 1 0 0 1 0 0 0 0 1];
     'testnull', [0 0]; %0 1 1 1 0 0 1 0 0 0 0 1];
-    'testA0Cunitball', [1 0]; %0 1 1 1 0 0 1 0 0 0 0 1];
+    'testA0Cunitball', [0 0]; %0 1 1 1 0 0 1 0 0 0 0 1];
     'testA0UInterval', [0 0]; %0 1 1 1 0 0 1 0 0 0 0 1];
-    'rot2d', [0 0]; %0 1 1 1 0 0 1 0 0 0 0 1];
+    'rot2d', [1 0]; %0 1 1 1 0 0 1 0 0 0 0 1];
     'demo3firstTest',  [0 0]; %1 0 1 0 1 0 1 1 0 1 1 0];
     'demo3secondTest', [0 0]; %0 0 0 0 0 0 1 0 0 0 0 0];
     'demo3thirdTest',  [0 0]; %0 0 0 1 0 1 1 0 0 0 0 0];
     'demo3fourthTest', [0 0]; %0 1 1 1 0 0 1 0 0 0 0 1];
+    'check', [0 0];
    % 'test2dbad',       [0 0 0 0 0 0 0 0 0 0 1 0 0 0]...
     };
 %
 nConfs = size(confCMat, 1);
 suiteList = {};
-%
+% 
 for iConf = 1:nConfs
     confName = confCMat{iConf, 1};
     confTestsVec = confCMat{iConf, 2};
@@ -32,7 +34,7 @@ for iConf = 1:nConfs
             'elltool.control.test.mlunit.ReachContTC',...
             ReachFactory(confName, crm, crmSys, true, false),...
             'marker',[confName,'_IsBackTrueIsEvolveFalse']);
-    end
+    end 
 %     if confTestsVec(1)
 %         suiteList{end + 1} = loader.load_tests_from_test_case(...
 %             'elltool.control.test.mlunit.ReachContTC',...
