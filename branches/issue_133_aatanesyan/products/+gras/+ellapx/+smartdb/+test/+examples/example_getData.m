60cf16dc989ea66b656fb73b1755ed23204c5d8e
anna.atanesyan.92@gmail.com <anna.atanesyan.92@gmail.com@fbfa2205-8925-0410-8e01-63a555b7045b>
2014-02-24 20:42:41 +0000

    issue 133 Enhancement: committing some changes

---
+++ branches/issue_133_aatanesyan/products/+gras/+ellapx/+smartdb/+test/+examples/example_getData.m
@@ -0,0 +1,21 @@
+% An example of GETDATA method's usage.
+nPoints=10;
+absTol=0.01;
+relTol=0.01;
+approxSchemaDescr='Internal';
+approxSchemaName='Internal';
+nDims=3;
+nTubes=3;
+lsGoodDirVec=[1;0;1];
+aMat=zeros(nDims,nPoints);
+timeVec=(1/nPoints):(1/nPoints):1;
+sTime=timeVec(randi(nPoints,1));
+approxType=gras.ellapx.enums.EApproxType.Internal;
+qArrayList=repmat({repmat(diag([1 2 3]),[1,1,nPoints])},...
+    1,nTubes);
+ltGoodDirArray=repmat(lsGoodDirVec,[1,nTubes,nPoints]);
+fromMatEllTube=gras.ellapx.smartdb.rels.EllTube.fromQArrays(...
+    qArrayList, aMat, timeVec,...
+    ltGoodDirArray, sTime, approxType, approxSchemaName,...
+    approxSchemaDescr, absTol, relTol);
+data = fromMatEllTube.getData();
